0d04edbcf3ef0f88eff3009c81669aa54e86a903
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-08-23 09:00:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // write js code here if required
+let pTag = document.querySelector('p');
+
+function getCurrentDateTime() {
+    let now = new Date();
+
+    let day = now.getDate();
+    let month = now.getMonth() + 1;
+    let year = now.getFullYear();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    let dateTimeString = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+
+    return dateTimeString;
+}
+
+setInterval(function() {
+    pTag.innerHTML = getCurrentDateTime();
+}, 1000);
